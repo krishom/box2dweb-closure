d61905e81d89563764f3861eb0f07ca6498cba0b
Mads Jon Nielsen <madsjon@gmail.com>
2013-03-19 11:32:37 +0100

    Added copyright notice in ropejoint.

--- src/dynamics/joints/ropejoint.js
+++ src/dynamics/joints/ropejoint.js
@@ -1,11 +1,34 @@
+/*
+ * Copyright (c) 2006-2007 Erin Catto http://www.gphysics.com
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ * Ported to AS3 by Allan Bishop http://allanbishop.com
+ * Ported to google closure box2d by Mads Jon Nielsen
+ */
+
 goog.provide('box2d.dynamics.joints.RopeJoint');
 
+goog.require('box2d.common.Settings');
+goog.require('box2d.common.math.Vec2');
 goog.require('box2d.dynamics.joints.Joint');
 
 /**
  * @constructor
  * @extends {box2d.dynamics.joints.Joint}
- * @param {!box2d.dynamics.joints.RopeJointDef} def
+ * @param {!box2d.dynamics.joints.RopeJointDef} def Rope definition.
  */
 box2d.dynamics.joints.RopeJoint = function(def) {
     goog.base(this, def);
@@ -227,14 +250,14 @@ box2d.dynamics.joints.RopeJoint.prototype.getReactionTorque = function(inv_dt) {
 };
 
 /**
- * @return {number}
+ * @return {number} Max length of the rope.
  */
 box2d.dynamics.joints.RopeJoint.prototype.getMaxLength = function() {
     return this.m_maxlength;
 };
 
 /**
- * @return {number}
+ * @return {number} Limit state.
  */
 box2d.dynamics.joints.RopeJoint.prototype.getLimitState = function() {
     return this.m_state;
--- src/dynamics/joints/ropejointdef.js
+++ src/dynamics/joints/ropejointdef.js
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2006-2007 Erin Catto http://www.gphysics.com
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ * Ported to AS3 by Allan Bishop http://allanbishop.com
+ * Ported to google closure box2d by Mads Jon Nielsen
+ */
+
 goog.provide('box2d.dynamics.joints.RopeJointDef');
 
 goog.require('box2d.dynamics.joints.JointDef');
@@ -6,11 +27,11 @@ goog.require('box2d.dynamics.joints.RopeJoint');
 /**
  * @constructor
  * @extends {box2d.dynamics.joints.JointDef}
- * @param {!box2d.dynamics.Body} bA
- * @param {!box2d.dynamics.Body} bB
- * @param {!box2d.common.math.Vec2} anchorB
- * @param {!box2d.common.math.Vec2} anchorA
- * @param {!number} maxlength
+ * @param {!box2d.dynamics.Body} bA Body a.
+ * @param {!box2d.dynamics.Body} bB Body b.
+ * @param {!box2d.common.math.Vec2} anchorA Anchor a.
+ * @param {!box2d.common.math.Vec2} anchorB Anchor b.
+ * @param {!number} maxlength Max length of the rope.
  */
 box2d.dynamics.joints.RopeJointDef = function(bA, bB, anchorA, anchorB, maxlength) {
     goog.base(this);
@@ -44,6 +65,10 @@ box2d.dynamics.joints.RopeJointDef = function(bA, bB, anchorA, anchorB, maxlengt
 };
 goog.inherits(box2d.dynamics.joints.RopeJointDef, box2d.dynamics.joints.JointDef);
 
+/**
+ * Creates a rope joint.
+ * @return {box2d.dynamics.joints.RopeJoint} A rope joint.
+ */
 box2d.dynamics.joints.RopeJointDef.prototype.create = function() {
     return new box2d.dynamics.joints.RopeJoint(this);
 };
